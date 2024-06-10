d4b90737c02d9fda5079a0196301c73fc49ed6d7
Sabari Kameswaran S <sabariedu19@gmail.com>
2024-06-10 22:23:39 +0530

    New task

--- Daily Task - 03/Daily Task - 10.06/array_with_even_and_odd.py
+++ Daily Task - 03/Daily Task - 10.06/array_with_even_and_odd.py
@@ -1,15 +1,27 @@
 n=int(input())
 arr1 = list(map(int,input().split()))
-# arr2 =[]
 def array_manipulation(arr1):
-    rev = 0
-    for i in range(0,n-1):
-        if arr1[i] == 0:
-            del arr1[i]
-        while i>0:
-            r=n%10
-            if r == 0:
-                del arr1[i]
-    print(arr1)
-
+    length=len(arr1)
+    if length == n:
+        for num in arr1:
+            if num == 0:
+                arr1.remove(num)
+            while num > 0 :
+                rev = num % 10
+                if rev == 0:
+                    arr1.remove(num)
+                num = num//10
+        print(arr1)
+        lenght1=len(arr1)
+        if lenght1%2 == 0:
+            mid3 = lenght1//2 - 1
+            mid2 = lenght1//2
+            even_mid=arr1[mid3]+arr1[mid2]
+            even_mid /=2
+            print(even_mid)
+        else:
+            mid1 = lenght1//2
+            print(arr1[mid1])
+    else:
+        print("Enter the Correct size of the array")
 array_manipulation(arr1)
