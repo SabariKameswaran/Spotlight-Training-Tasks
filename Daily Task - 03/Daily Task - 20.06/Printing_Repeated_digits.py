46c320e7f0d7cbe52c03b5e0675012d7f7b2bb96
Sabari Kameswaran S <sabariedu19@gmail.com>
2024-06-22 17:45:49 +0530

    New

--- Daily Task - 03/Daily Task - 20.06/Printing_Repeated_digits.py
+++ Daily Task - 03/Daily Task - 20.06/Printing_Repeated_digits.py
@@ -1,5 +1,3 @@
-n=int(input("Enter the Number : "))
-arr1=[]
 def count_repeat(n,arr1):
     Value=n%10
     if Value>0:
@@ -7,16 +5,26 @@ def count_repeat(n,arr1):
     num=n//10
     if n>0:
         return count_repeat(num,arr1)
-count_repeat(n,arr1)
-arr2=[]
-count=0
-for i in arr1:
-    if i not in arr2:
-        arr2.append(i)
-print(arr2)
-for j in arr2:
+def frequency():
+    n=int(input("Enter the Number : "))
+    arr1=[]
+    count_repeat(n,arr1)
+    arr2=[]
+    arr3=[]
     count=0
-    for s in arr1:
-        if s==j:
-            count=count+1
-    print(j," = ",count)
+    for i in arr1:
+        if i not in arr2:
+            arr2.append(i)
+    for j in arr2:
+        count=0
+        for s in arr1:
+            if s==j:
+                count=count+1
+        # print(j,"=",count)
+        arr3.append(count)
+    m=set(arr3)
+    if len(m)==1:
+        print("Stable Number")
+    else:
+        print("Unstable Number")
+frequency()
